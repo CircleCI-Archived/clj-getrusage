0b725db6c3577beb7e7b6825bce989ffa4534baf
Lizzie Dixon <_@lizzie.io>
2016-05-20 14:56:24 -0700

    Try updating our dependencies.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/circleci/clj-getrusage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.java.dev.jna/jna "3.2.7"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.java.dev.jna/jna "4.2.2"]]
   :java-source-paths ["src/java"])
